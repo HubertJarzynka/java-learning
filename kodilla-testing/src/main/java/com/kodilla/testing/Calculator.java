48c4b1ca9a1abdbadf48b714f6c299bb43682c50
HubertJarzynka <hubjarz@gmail.com>
2022-03-10 18:44:37 +0100

    6.2 pierwszy test jednostkowy V2

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/Calculator.java
@@ -0,0 +1,10 @@
+package com.kodilla.testing;
+
+public class Calculator {
+    public int add(int i, int i1) {
+        return i+i1;
+    }
+    public int subtract(int i, int i1) {
+        return i-i1;
+    }
+}
--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,26 @@
 package com.kodilla.testing;
 
 public class TestingMain {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws java.lang.Exception {
         System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+        Calculator calculator = new Calculator();
+        int addResult = calculator.add(180, 19);
+        int subResult = calculator.subtract(200, 150);
+
+        if (addResult == 199){
+            System.out.println("Calculator test OK");
+        } else {
+            System.out.println("Calculator has error!");
+        }
+        if (subResult == 50){
+            System.out.println("Calculator test OK");
+        } else {
+            System.out.println("Calculator has error!");
+        }
     }
 }
+
+
+
+
