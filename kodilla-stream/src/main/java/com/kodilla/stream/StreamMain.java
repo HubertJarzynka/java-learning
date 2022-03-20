315f66d63bee7ef3f82a2a9f4d366270d9450ac4
HubertJarzynka <hubjarz@gmail.com>
2022-03-20 09:10:28 +0100

    7.1 Upiększacz tekstów

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,27 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.lambda.*;
+import com.kodilla.stream.reference.FunctionalCalculator;
+
+public class StreamMain {
+    public static void main(String[] args) {
+
+        Processor processor = new Processor();
+        processor.execute(() -> System.out.println("This is an example text!"));
+
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+        expressionExecutor.executeExpression(10, 5, FunctionalCalculator::addAToB);
+        expressionExecutor.executeExpression(10, 5, FunctionalCalculator:: subBFromA);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        poemBeautifier.beautify("backpack", String::toUpperCase);
+        poemBeautifier.beautify("BACKPACK", (text) -> text.toLowerCase());
+        poemBeautifier.beautify("backpack", (text) -> "new " + text);
+        poemBeautifier.beautify("backpack", (text) -> text.substring(0, 4));
+        poemBeautifier.beautify("backpack", (text) -> text.replace("b","B"));
+
+    }
+}
