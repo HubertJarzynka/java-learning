655aec38d5d0c6d34658eb5d48110d7923b0fc7c
HubertJarzynka <hubjarz@gmail.com>
2022-03-20 10:24:07 +0100

    7.2 Niemutowalny użytkownik forum

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.*;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
@@ -23,5 +24,7 @@ public class StreamMain {
         poemBeautifier.beautify("backpack", (text) -> text.substring(0, 4));
         poemBeautifier.beautify("backpack", (text) -> text.replace("b","B"));
 
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
