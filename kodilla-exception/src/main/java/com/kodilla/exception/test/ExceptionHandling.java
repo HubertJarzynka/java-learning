1773a8cefd273bb44894ee7ae78e8abb575817a8
HubertJarzynka <hubjarz@gmail.com>
2022-03-28 19:00:29 +0200

    8.3 obsługa wyjątku

--- kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -1,4 +1,18 @@
 package com.kodilla.exception.test;
 
+import java.io.IOException;
+
 public class ExceptionHandling {
+
+    public static void main(String[] args) throws IOException {
+
+        try {
+            SecondChallenge challenge = new SecondChallenge();
+            challenge.probablyIWillThrowException(2, 1.5);
+        } catch (Exception e) {
+            System.out.println("Problem, invalid method arguments");
+        } finally {
+            System.out.println("Finish");
+        }
+    }
 }
--- kodilla-exception/src/test/java/com/kodilla/exception/io/SecondChallengeTestSuite.java
+++ kodilla-exception/src/test/java/com/kodilla/exception/io/SecondChallengeTestSuite.java
@@ -1,4 +1,18 @@
 package com.kodilla.exception.io;
 
+import com.kodilla.exception.test.SecondChallenge;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class SecondChallengeTestSuite {
+
+    @Test
+    void testIWillThrowException() {
+    // Given
+    SecondChallenge challenge = new SecondChallenge();
+
+    // When & Then
+    assertThrows(Exception.class, () -> challenge.probablyIWillThrowException(3, 1.5));
+    assertThrows(Exception.class, () -> challenge.probablyIWillThrowException(0.9, 1.5));
+    }
 }
